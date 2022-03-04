c802549a1375e99149114702aaa1ad2f6544ac3a
ElperdonXV <davidemartinografico@gmail.com>
2022-03-04 16:56:15 +0100

    work on seeder

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Model\Tag;
+use Illuminate\Support\Str;
 class TagSeeder extends Seeder
 {
     /**
@@ -11,6 +12,19 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags=[
+            'animals',
+            'something',
+            'roma',
+            'parigi',
+            'milano',
+        ];
+
+        foreach($tags as $tag){
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag, '-');
+            $newTag->save();
+        }
     }
 }
