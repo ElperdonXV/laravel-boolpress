f66c2e0442acc2ec6cfc09574b26f3c1cb893f20
ElperdonXV <davidemartinografico@gmail.com>
2022-03-08 15:47:24 +0100

    ok welcome blade php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('guest.index');
+    return view('guest.welcome');
+});
 
 Auth::routes();
 
