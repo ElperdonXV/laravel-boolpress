9681fca2965117aa6031b54a2c4c3d483e4b520d
ElperdonXV <davidemartinografico@gmail.com>
2022-03-10 16:26:53 +0100

    tag controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 use App\Model\Post;
 
 class PostController extends Controller
@@ -22,6 +23,39 @@ class PostController extends Controller
         $posts = Post::inRandomOrder()->limit(4)->get();
         return response()->json([
             'response' => true,
+            'results' => [
+               'data'=>$posts
+            ]
+        ]);
+    }
+
+    public function search(Request $request){
+        $data = $request->all();
+
+        //apriamo una chiamata eloquent senza chiuderla
+        $posts = Post::where('id', '>=', 1);
+         //se abbiamo orderbycolumn e orderbysort in $data
+        //li usiamo per ordinare
+        if (
+            array_key_exists('orderbycolumn', $data) &&
+            array_key_exists('orderbysort', $data)
+        ) {
+            $posts->orderBy($data['orderbycolumn'], $data['orderbysort']);
+        }
+
+         if (array_key_exists('tags', $data)) {
+            foreach ($data['tags'] as $tag) {
+                //fa una join per controllare i tag che sono associati al product
+                $posts->whereHas('tags', function (Builder $query) use ($tag) {
+                    $query->where('name', '=', $tag);
+                });
+            }
+        }
+
+        $posts = $posts->with(['tags', 'category'])->get();
+        return response()->json([
+            'response' => true,
+            'count' =>  $posts->count(),
             'results' => [
                'data'=>$posts
             ]
