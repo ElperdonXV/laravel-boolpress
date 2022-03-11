7be874465565c39ccb727c1ee128446d4b90f0ff
ElperdonXV <davidemartinografico@gmail.com>
2022-03-11 15:52:32 +0100

    all done

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -6,6 +6,7 @@
             <h1>Home</h1>
           </div>
         </div>
+        <Loading v-if="loading"/>
         <Main :cards="cards" @changePage="changePage($event)"></Main>
       </div>
   </div>
@@ -13,14 +14,17 @@
 
 <script>
 import Axios from "axios";
+import Loading from "../components/Loading.vue";
 import Main from '../components/Main.vue';
   export default {
     name: 'Home',
     components: {
-      Main
+      Main,
+      Loading
     },
     data() {
       return {
+        loading:false,
         cards: {
           posts: null,
           next_page_url: null,
@@ -39,13 +43,17 @@ import Main from '../components/Main.vue';
         }
       },
       getPosts(url){
+         this.loading = true;
+          setTimeout(() => {
           Axios.get(url).then(
             (result) => {
               console.log(result);
               this.cards.posts = result.data.results.data;
               this.cards.next_page_url = result.data.results.next_page_url;
               this.cards.prev_page_url = result.data.results.prev_page_url;
+              this.loading = false;
             });
+        }, 2000);
       }
       
     }
